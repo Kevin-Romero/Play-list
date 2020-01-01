167c25ba76639d2d5ab59cc02ea4719eebc45739
Kevin-Romero <kevin.rgki@gmail.com>
2019-12-31 16:19:23 -0800

    Primera modificacion de woodu fase beta

---
+++ js/play-list.js
@@ -0,0 +1,146 @@
+ var canciones =[
+    {nombre: 'Lenguaje de amor', autor: 'Alex campos', link: 'https://www.youtube.com/watch?v=oWOvQh111bA', categoria: 'Rock, Pop'},
+            
+    {nombre: 'Yo no temere', autor: 'Alex campos', link: 'https://www.youtube.com/watch?v=5m-Mgsk95Yk', categoria: 'Rock, Pop'},
+            
+    {nombre: 'Todo es vanidad', autor: 'Jahlove', link: 'https://www.youtube.com/watch?v=AlJAW8aH6pg', categoria: 'Reggae'},
+            
+    {nombre: 'No tiene prisa', autor: 'Alex campos', link: 'https://www.youtube.com/watch?v=3J0cgAx1L6Q', categoria: 'Rock, Pop, Balada'},
+            
+    {nombre: 'Justo a tiempo', autor: 'Funky', link: 'https://www.youtube.com/watch?v=nVQeeii_urI', categoria: 'Pop, Balada'},
+            
+    {nombre: 'Pancho y pancha', autor: 'Desconocido', link: '', categoria: 'Ranchera'},
+            
+    {nombre: 'Me falta corazon', autor: 'Miguel balboa', link: 'https://www.youtube.com/watch?v=Fe5_DALhI1k', categoria: 'Rock, Pop'},
+            
+    {nombre: 'La casa', autor: 'Jose holguin', link: 'https://www.youtube.com/watch?v=nSU1ZlgMoEU', categoria: 'Rock, Pop, Balada'},
+            
+    {nombre: 'Fue mi jesus', autor: 'Jose holguin', link: 'https://www.youtube.com/watch?v=d9a9RE_6-H4&feature=youtu.be&t=10', categoria: 'Pop'},
+            
+    {nombre: 'Que gente tan loca', autor: 'Jose holguin', link: 'https://www.youtube.com/watch?v=UqAZx1Kqy68', categoria: 'Pop, Rock and roll'},
+            
+    {nombre: 'Ven y escucha este canto', autor: 'Jose holguin', link: 'https://www.youtube.com/watch?v=KNHLS0A48u4', categoria: 'Pop, Rock and roll'},
+            
+    {nombre: 'De sol a sol', autor: 'Deluz', link: 'https://www.youtube.com/watch?v=4EYB4b03bKI', categoria: 'Rock, Pop'},
+            
+    {nombre: 'Luz y sal', autor: 'Funky', link: 'https://youtu.be/mS1uElTYS-Y?t=11', categoria: 'Rock, Pop'},
+            
+    {nombre: 'Mendigo', autor: 'Vico C ft Hector Delgado', link: 'https://www.youtube.com/watch?v=14oU3vuRpo8', categoria: 'Rap, Pop'},
+            
+    {nombre: 'Donde?', autor: 'Miguel Balboa', link: 'https://www.youtube.com/watch?v=anMvfFVQWXo', categoria: 'Pop, Balada'},
+            
+    {nombre: 'Un dia mas', autor: 'Miguel Balboa', link: 'https://www.youtube.com/watch?v=9UwhEsSnk4Y', categoria: 'Pop, Balada'},
+            
+    {nombre: 'Que eres para mi?', autor: 'Alex campos', link: 'https://www.youtube.com/watch?v=Uf1ALItpAOY', categoria: 'Rock, Pop'},
+            
+    {nombre: 'Solo para ti', autor: 'Fuego ardiente', link: 'https://www.youtube.com/watch?v=1FB1yhtovnQ', categoria: 'Reggae, SKA'},
+            
+    {nombre: 'Hosanna', autor: 'Marcos barrientos', link: 'https://www.youtube.com/watch?v=umPWpA8-FXU', categoria: 'Rock, Pop'},
+            
+    {nombre: 'Soy amado', autor: 'La historia', link: 'https://youtu.be/f1hjgPCRMVA?t=11', categoria: 'Ranchera, Norteña'},
+            
+    {nombre: 'Buscando', autor: 'Miguel balboa', link: 'https://www.youtube.com/watch?v=9dxdGzIj_Hw', categoria: 'Rock, Pop'},
+            
+    {nombre: 'La niña mujer', autor: 'Juan jose jimenes', link: 'https://www.youtube.com/watch?v=QcGu4BxPMxk', categoria: 'Pop'},
+            
+    {nombre: 'Una prostituta', autor: 'Dc reto', link: 'https://www.youtube.com/watch?v=BgsDVWeXUFM', categoria: 'Pop, Balada'},
+            
+    {nombre: 'Transformame', autor: 'Strike 3', link: 'https://www.youtube.com/watch?v=1_uOjHW9LdE', categoria: 'Rock, Pop, Balada'},
+            
+    {nombre: 'Bajo la sombra', autor: 'Los benditos', link: 'https://www.youtube.com/watch?v=xiAMV9XhKGU', categoria: 'Cumbia'},
+            
+    {nombre: 'Dame', autor: 'Adrian roberto romero', link: 'https://www.youtube.com/watch?v=V6K8LHvJlc0', categoria: 'Pop, Balada'},
+            
+    {nombre: '25 de diciembre', autor: 'Desconocido (Minuto 5:43)', link: 'https://www.facebook.com/100009051543227/videos/vb.100009051543227/2402809883364040/?type=2&video_source=user_video_tab', categoria: 'Pop, Reggae'},
+            
+    {nombre: 'No me soltaras', autor: 'Rojo', link: 'https://youtu.be/BByIL33s5W0?t=25', categoria: 'Rock, Pop, Balada'},
+            
+    {nombre: 'Bajo tus alas', autor: 'Pepe lopez band', link: 'https://youtu.be/BnbJru1CGq8?t=7', categoria: 'Rock, Pop, Balada'},
+            
+    {nombre: 'Se nota en tus ojos', autor: 'Funky', link: 'https://www.youtube.com/watch?v=mTmq3-DmBmM', categoria: 'Pop, Balada'},
+            
+    {nombre: 'Un secreto', autor: 'Manny montes', link: 'https://youtu.be/HNbDUaP9yb8?t=7', categoria: 'Pop, Balada'},
+            
+    {nombre: 'Para cantarte', autor: 'Strike 3', link: 'https://www.youtube.com/watch?v=3OfgjyZ0dfU', categoria: 'Rock, Pop, Balada'},
+            
+    {nombre: 'Alegra mi vida', autor: 'Rabito', link: 'https://www.youtube.com/watch?v=nVJLBpq7M-U', categoria: 'Reggae'},
+            
+    {nombre: 'Al taller del maestro', autor: 'Alex campos', link: 'https://www.youtube.com/watch?v=xepY2ULKwYQ', categoria: 'Pop, Balada'},
+            
+    {nombre: 'Abre los cielos', autor: 'Redimi2 ft miel san marcos', link: 'https://youtu.be/HmW59XLPDTM?t=69', categoria: 'Pop'},
+            
+    {nombre: 'Liberame', autor: 'Samaritan revival', link: 'https://youtu.be/aBRSMYfojKA?t=18', categoria: 'Rock, Pop, Balada'},
+            
+    {nombre: 'Loco por tu amor', autor: 'Os almirantes', link: 'https://www.youtube.com/watch?v=tHrMn4FyF8k', categoria: 'SKA, Balada'},
+            
+    {nombre: 'Explosion', autor: 'Vico c', link: 'https://youtu.be/1KwUlvi0e_A?t=8', categoria: 'Reggae'},
+            
+    {nombre: 'Me dijo', autor: 'Alex campos', link: 'https://www.youtube.com/watch?v=E41UyN9uK-M', categoria: 'Reggae, SKA'},
+            
+    {nombre: 'Necesitamos de el', autor: 'Vico C', link: 'https://www.youtube.com/watch?v=GPHMSwRpbcI', categoria: 'Reggae'},
+            
+    {nombre: 'Paracaidas', autor: 'Miguel balboa', link: 'https://www.youtube.com/watch?v=PRiwo7Mz8Nk', categoria: 'Rock, Pop'},
+            
+    {nombre: 'Superheroe', autor: 'Vico c', link: 'https://youtu.be/gJYbrKHto00?t=18', categoria: 'Pop, Reggae'},
+            
+    {nombre: 'Anoche llegaste tarde', autor: 'Funky', link: 'https://www.youtube.com/watch?v=8sM2clK1e4k', categoria: 'Pop, Rap, Balada'},
+            
+    {nombre: 'Defendere mi fe', autor: 'Alpha union', link: 'https://www.youtube.com/watch?v=hfzKikaatuE', categoria: 'Pop, Balada'},
+            
+    {nombre: 'Ya no quiero vivir asi', autor: 'Contagious', link: 'https://www.youtube.com/watch?v=hfzKikaatuE', categoria: 'Pop, Balada'},
+            
+    {nombre: 'Dandote vida', autor: 'Vico c', link: 'https://www.youtube.com/watch?v=Ps1SQeVlCZk', categoria: 'Pop, Balada'},
+            
+    {nombre: 'El escenario', autor: 'Manny montes', link: 'https://youtu.be/32j1s8emIuQ?t=38', categoria: 'Pop, Reggae'},
+            
+    {nombre: 'Empezar de nuevo', autor: 'Kike pavon', link: 'https://www.youtube.com/watch?v=KdJn1ZSWbXo', categoria: 'Pop'},
+            
+    {nombre: 'Segunda oportunidad', autor: 'Maritza barreñada', link: 'https://www.youtube.com/watch?v=Hhu59TjZkwc', categoria: 'Pop'},
+            
+    {nombre: 'Siempre conmigo', autor: 'Sacrificio', link: 'https://www.youtube.com/watch?v=zSztGtIxe5I', categoria: 'Rock, Balada'},
+            
+    {nombre: 'Mi niñez', autor: 'Ariel ramirez', link: 'https://www.youtube.com/watch?v=GGbPyFSHJW4', categoria: 'Pop, Balada'},
+             
+    {nombre: 'Sueño de morir', autor: 'Alex campos', link: 'https://youtu.be/aomWXAFNldQ?t=25', categoria: 'Pop, Balada'},
+            
+    {nombre: 'Huesos secos', autor: 'Mauricio fuentes', link: 'https://www.youtube.com/watch?v=_VsJUHGwE24', categoria: 'Pop, Balada'}
+]
+     
+    var search = document.getElementById('search');
+    var btnSearch = document.getElementById('btn-search');
+    var resultado = document.getElementById('search-body');
+    var numbreSong = document.getElementById('number-song-container');    
+
+    var numeroCanciones = canciones.length;
+                
+    numbreSong.innerHTML += '<p>'+numeroCanciones+'</p>';
+        
+    function filtrar(){
+        //console.log(search.value);
+            
+        resultado.innerHTML = '';
+            
+
+            
+        var texto = search.value.toLowerCase();
+        for(var cancion of canciones){
+            var nombre = cancion.nombre.toLowerCase();
+            var autor = cancion.autor.toLowerCase();
+            var categoria = cancion.categoria.toLowerCase();                
+                
+            if(nombre.indexOf(texto) !== -1 || autor.indexOf(texto) !== -1 || categoria.indexOf(texto) !== -1){
+                resultado.innerHTML += '<a href="'+cancion.link+'" target="_blank" >'+cancion.nombre+' - '+cancion.autor+' /  '+cancion.categoria+'</a>';
+            }
+            else{
+                resultado.innerHTML += ''; 
+            }
+                
+        }
+    }
+        
+    btnSearch.addEventListener('click',filtrar);
+        
+    search.addEventListener('keyup', filtrar);
+        
+    filtrar();
+        
